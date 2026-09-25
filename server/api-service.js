58ca6db98a75837f5f472148b5809347eabe2377
VoidcallerOC <voidcalleroc@gmail.com>
2026-09-25 16:57:42 +0000

    Restore api-service and import ARTIST_SELECT from artist-verified-select.

--- server/api-service.js
+++ server/api-service.js
@@ -1,1 +1,8 @@
-PLACEHOLDER
+import { randomUUID } from "node:crypto";
+import { ApiError } from "./api-errors.js";
+import { assertWalletMatches, requireWalletAuth } from "./api-runtime.js";
+import { checkDatabaseHealth } from "./db.js";
+import { reportIndexedContracts } from "./indexer-contracts.js";
+import { chainId, nonNegativeBigInt, positiveBigInt, requiredText, walletAddress } from "./validation.js";
+import { isHiddenPublicArtist } from "../src/lib/summit-demo.js";
+import { ARTIST_SELECT } from "./artist-verified-select.js";
